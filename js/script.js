78dff31b5c9b7712d99651cb97b29306f3b2a18a
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-02-12 18:34:09 +0800

    Add an auto complete of place using google map api

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
-let map;
+let map,directionsService,directionsRenderer;
+let originAC,destinationAC;
 
 function initMap(){
   map = new google.maps.Map(document.getElementById("map"),{
     center:{lat: 14.5794,lng: 121.0359},
     zoom: 8,
   });
+
+  directionsService = new google.maps.DirectionsService();
+  directionsRenderer = new google.maps.DirectionsRenderer();
+  directionsRenderer.setMap(map);
+  
+  originAC = new google.maps.places.Autocomplete(
+    document.getElementById('origin')
+  );
+
+  destinationAC = new google.maps.places.Autocomplete(
+    document.getElementById('destination')
+  );
 }
